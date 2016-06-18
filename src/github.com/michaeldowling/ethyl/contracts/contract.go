ee6bff687cb46d594476dbccc2363fdab57810e3
Michael Dowling <michael.dowling@minderupt.com>
2016-06-17 18:22:39 -0700

    Added ABI parsing for Contract Definitions

--- src/github.com/michaeldowling/ethyl/contracts/contract.go
+++ src/github.com/michaeldowling/ethyl/contracts/contract.go
@@ -1,13 +1,44 @@
 package contracts
 
+import (
+    "encoding/json"
+    "log"
+)
+
 type Contract struct {
-    ABI     string
+    Abi     ABI
     Address string
+}
+
+type ABI struct {
+    InterfaceDefinitions []ContractInterfaceDefinition
+}
+
+type ContractInterfaceDefinition struct {
+    Constant bool `json:"constant"`
+    Name     string `json:"name"`
+    Type     string `json:"type"`
+    Inputs   []InputOutputDefinition `json:"inputs"`
+    Outputs  []InputOutputDefinition `json:"outputs"`
+}
+
+type InputOutputDefinition struct {
+    Name         string `json:"name"`
+    EthereumType string `json:"type"`
 }
 
 func DefineContract(abi string) (Contract, error) {
 
-    return Contract{ABI:abi}, nil;
+    var contract Contract;
+    var abiDefinition ABI;
+    contractErr := json.Unmarshal([]byte(abi), &abiDefinition.InterfaceDefinitions);
+    if (contractErr != nil) {
+        log.Printf("Unable to define contract:  %v", contractErr);
+        return contract, contractErr;
+    }
+
+    contract.Abi = abiDefinition;
+    return contract, nil;
 
 }
 
--- src/github.com/michaeldowling/ethyl/contracts/contract_test.go
+++ src/github.com/michaeldowling/ethyl/contracts/contract_test.go
@@ -1,10 +1,23 @@
 package contracts
 
-import "testing"
+import (
+    "testing"
+    "io/ioutil"
+    "github.com/stretchr/testify/assert"
+)
 
 func TestContract_DefineContract(t *testing.T) {
 
+    // Load ABI
+    contents, fileErr := ioutil.ReadFile("../../../../../test_contracts/ballot_test_abi.json");
+    assert.Nil(t, fileErr);
+    assert.NotNil(t, contents);
 
+    contract, contractErr := DefineContract(string(contents));
+    assert.Nil(t, contractErr);
+    assert.NotNil(t, contract);
+
+    assert.Equal(t, "delegate", contract.Abi.InterfaceDefinitions[0].Name);
 
 
 }
